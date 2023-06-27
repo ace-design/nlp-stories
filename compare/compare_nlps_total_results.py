25e276dfb861ada18bab37cf57eda16979801cb0
Sathurshan <arulmohs@mcmaster.ca>
2023-06-27 06:07:18 -0400

    Modified scripts and folder structure for GPT,VN and CRF

--- compare/compare_nlps_total_results.py
+++ compare/compare_nlps_total_results.py
@@ -6,15 +6,13 @@ import seaborn as sns
 import sys
 
 def main():
-    simple_path, ecmfa_vn_path, visual_narrator_path, chatgpt_path, crf_path, saving_path, comparison_type, number_dataset, title_name = command()
+    visual_narrator_path, chatgpt_path, crf_path, saving_path, comparison_type, number_dataset, title_name = command()
     datasets_label = get_datasets_labels(comparison_type)
 
-    simple_data = extract_data(simple_path, number_dataset)
-    ecmfa_vn_data = extract_data(ecmfa_vn_path, number_dataset)
     visual_narrator_data = extract_data(visual_narrator_path, number_dataset)
     chatgpt_data = extract_data(chatgpt_path, number_dataset)
 
-    formatted_data = format_data(datasets_label, number_dataset, simple_data, ecmfa_vn_data, visual_narrator_data, chatgpt_data, crf_path)
+    formatted_data = format_data(datasets_label, number_dataset, visual_narrator_data, chatgpt_data, crf_path)
 
     persona_precision, persona_recall, persona_f_measure,entity_precision, entity_recall, entity_f_measure, action_precision, action_recall, action_f_measure = formatted_data
 
@@ -29,8 +27,6 @@ def command():
     Runs the command line inputs
 
     Returns:
-    args.load_simple_path (str): Path to the simple data csv file to be loaded
-    args.load_ecmfa_vn_path (str): Path to the ecmfa_vn data csv file to be loaded
     args.load_visual_narrator_path (str): Path to the visual narrator data csv file to be loaded
     args.load_chatgpt_path (str): Path to the chatgpt data csv file to be loaded 
     args.load_crf_path (str): Path to the crf data csv file to be loaded, it is NONE if it is not given
@@ -43,8 +39,6 @@ def command():
     wrong file order: raises exception
     '''
     parser = argparse.ArgumentParser(description = "This program will compare all the nlp results and output visulations of the results")
-    parser.add_argument("load_simple_path", type = str, help = "path of simple csv file")
-    parser.add_argument("load_ecmfa_vn_path", type = str, help = "path of ecmfa_vn csv file")
     parser.add_argument("load_visual_narrator_path", type = str, help = "path of visual narrator csv file")
     parser.add_argument("load_chatgpt_path", type = str, help = "path of ChatGPT csv file")
     parser.add_argument("--load_crf_path", nargs="?", type = str, help = "path of crf csv file")
@@ -54,32 +48,28 @@ def command():
     
     args = parser.parse_args()
 
-    if not(args.load_simple_path.endswith(".csv")) or not(args.load_ecmfa_vn_path.endswith(".csv")) or not(args.load_visual_narrator_path.endswith(".csv")) or not(args.load_chatgpt_path.endswith(".csv")) or (args.load_crf_path != None and not(args.load_crf_path.endswith(".csv"))):
+    if not(args.load_visual_narrator_path.endswith(".csv")) or not(args.load_chatgpt_path.endswith(".csv")) or (args.load_crf_path != None and not(args.load_crf_path.endswith(".csv"))):
         sys.tracebacklimit = 0
         raise Exception ("Incorrect input file type. File type is .csv")
 
-    if "strict" in args.load_simple_path and "strict" in args.load_ecmfa_vn_path and "strict" in args.load_visual_narrator_path and "strict" in args.load_chatgpt_path:
+    if  "strict" in args.load_visual_narrator_path and "strict" in args.load_chatgpt_path:
         comparison_type = "Strict Comparison"
         saving_name = "strict_" + args.save_file_name
-    elif "inclusion" in args.load_simple_path and "inclusion" in args.load_ecmfa_vn_path and "inclusion" in args.load_visual_narrator_path and "inclusion" in args.load_chatgpt_path:
+    elif "inclusion" in args.load_visual_narrator_path and "inclusion" in args.load_chatgpt_path:
         comparison_type = 'Inclusion Comparison'
         saving_name = "inclusion_" + args.save_file_name
-    elif "relaxed" in args.load_simple_path and "relaxed" in args.load_ecmfa_vn_path and "relaxed" in args.load_visual_narrator_path and "relaxed" in args.load_chatgpt_path:
+    elif "relaxed" in args.load_visual_narrator_path and "relaxed" in args.load_chatgpt_path:
         comparison_type = "Relaxed Comparison"
         saving_name = "relaxed_" + args.save_file_name
     else:
         sys.tracebacklimit = 0 
         raise Exception("Incompatible combination. All files must be evaluated by same comparison mode")
 
-    if not("simple" in args.load_simple_path) or not("ecmfa_vn" in args.load_ecmfa_vn_path) or not("visual_narrator" in args.load_visual_narrator_path) or not("chatgpt" in args.load_chatgpt_path):
+    if not("visual_narrator" in args.load_visual_narrator_path) or not("chatgpt" in args.load_chatgpt_path):
         sys.tracebacklimit = 0
-        raise Exception ("Incorrect order of input file. First file is simple, then ecmfa_vn, and then visual narrator")
+        raise Exception ("Incorrect order of input file. First file is visual narrator, then chatgpt")
 
     try:
-        load_file = open(args.load_simple_path)
-        load_file.close()
-        load_file = open(args.load_ecmfa_vn_path)
-        load_file.close()
         load_file = open(args.load_visual_narrator_path)
         load_file.close()
         load_file = open(args.load_chatgpt_path)
@@ -111,10 +101,10 @@ def command():
         print("File or directory does not exist")
         raise
     else:
-        data = pd.read_csv(args.load_simple_path)
+        data = pd.read_csv(args.load_visual_narrator_path)
         number_dataset = len(data)
 
-        return args.load_simple_path, args.load_ecmfa_vn_path, args.load_visual_narrator_path, args.load_chatgpt_path ,args.load_crf_path, save_file_path, comparison_type, number_dataset, title
+        return  args.load_visual_narrator_path, args.load_chatgpt_path ,args.load_crf_path, save_file_path, comparison_type, number_dataset, title
 
 def extract_data (path, number_dataset):
     '''
@@ -174,15 +164,13 @@ def get_datasets_labels(comparison_type):
 
     return datasets_label
 
-def format_data(datasets_label, number_dataset, simple_data, ecmfa_vn_data, visual_narrator_data, chatgpt_data, crf_path):
+def format_data(datasets_label, number_dataset, visual_narrator_data, chatgpt_data, crf_path):
     '''
     formats the data so that it can be easily plotted onto the graphs
 
     Parameters:
     datasets_label (list): all the datsets that are in the csv files
     number_dataset (int): the number of datsets in the csv files
-    simple_data(2D list): the final data results from simple nlp
-    ecmfa_vn_data(2D list): the final data results from ecmfa_vn nlp
     visual_narrator_data(2D list): the final data results from visual narrator nlp
     chatgpt_data(2D list): the final data results from ChatGPT nlp
     crf_path (str): path to crf csv files or is NONE if not given 
@@ -194,27 +182,23 @@ def format_data(datasets_label, number_dataset, simple_data, ecmfa_vn_data, visu
 
     if crf_path == None:
         for i in range(9):
-            rounded_simple = round_data(simple_data[i].values.tolist())
-            rounded_ecmfa_vn = round_data(ecmfa_vn_data[i].values.tolist())
             rounded_visual_narrator = round_data(visual_narrator_data[i].values.tolist())
             rounded_chatgpt = round_data(chatgpt_data[i].values.tolist())
 
-            formatted_data.append(pd.DataFrame({ "Dataset": datasets_label * 4,
-                                "Data": rounded_simple + rounded_ecmfa_vn + rounded_visual_narrator + rounded_chatgpt,
-                                "nlp": ["Simple"]*number_dataset + ["ECMFA-VN"]*number_dataset + ["Visual Narrator"]*number_dataset + ["ChatGPT"]*number_dataset}))
+            formatted_data.append(pd.DataFrame({ "Dataset": datasets_label * 2,
+                                "Data":  rounded_visual_narrator + rounded_chatgpt,
+                                "nlp":  ["Visual Narrator"]*number_dataset + ["ChatGPT"]*number_dataset}))
     else:
         crf_data = extract_data(crf_path, number_dataset)
 
         for i in range(9):
-            rounded_simple = round_data(simple_data[i].values.tolist())
-            rounded_ecmfa_vn = round_data(ecmfa_vn_data[i].values.tolist())
             rounded_visual_narrator = round_data(visual_narrator_data[i].values.tolist())
             rounded_chatgpt = round_data(chatgpt_data[i].values.tolist())
             rounded_crf = round_data(crf_data[i].values.tolist())
 
-            formatted_data.append(pd.DataFrame({ "Dataset": datasets_label * 5,
-                                "Data": rounded_simple + rounded_ecmfa_vn + rounded_visual_narrator + rounded_chatgpt + rounded_crf,
-                                "nlp": ["Simple"]*number_dataset + ["ECMFA-VN"]*number_dataset + ["Visual Narrator"]*number_dataset + ["ChatGPT"]*number_dataset + ["CRF"] * number_dataset}))
+            formatted_data.append(pd.DataFrame({ "Dataset": datasets_label * 3,
+                                "Data": rounded_visual_narrator + rounded_chatgpt + rounded_crf,
+                                "nlp": ["Visual Narrator"]*number_dataset + ["ChatGPT"]*number_dataset + ["CRF"] * number_dataset}))
 
     return formatted_data
 
@@ -240,9 +224,9 @@ def create_final_bargraph(precision_results,recall_results, f_measure_results, t
     graph, (precision_plot, recall_plot, f_measure_plot) = plt.subplots(3, 1, figsize=(20, 6), sharex=True)
 
     if crf_path == None:
-        palette ={"Simple": plt.cm.Pastel1(0), "ECMFA-VN": plt.cm.Pastel1(1), "Visual Narrator": plt.cm.Pastel1(2), "ChatGPT": plt.cm.Pastel1(3)}
+        palette ={"Visual Narrator": plt.cm.Pastel1(0), "ChatGPT": plt.cm.Pastel1(1)}
     else:
-        palette ={"Simple": plt.cm.Pastel1(0), "ECMFA-VN": plt.cm.Pastel1(1), "Visual Narrator": plt.cm.Pastel1(2), "ChatGPT": plt.cm.Pastel1(3), "CRF": plt.cm.Pastel1(4)}
+        palette ={"Visual Narrator": plt.cm.Pastel1(0), "ChatGPT": plt.cm.Pastel1(1), "CRF": plt.cm.Pastel1(2)}
 
     precision = sns.barplot(x= "Dataset", y= "Data", hue = "nlp",data = precision_results, ax = precision_plot, palette = palette, ci = None)
     precision.set(xlabel=None)
